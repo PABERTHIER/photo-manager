7f1a5522249e8a0d89e0350bf730f18149879648
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2023-08-12 17:11:50 +0200

    Improving FileNameConverterTests to be more accurate

---
+++ PhotoManager/PhotoManager.Tests/Unit/UI/Converters/FileNameConverterTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using PhotoManager.Domain;
+using PhotoManager.UI.Converters;
+using System.Globalization;
+
+namespace PhotoManager.Tests.Unit.UI.Converters;
+
+[TestFixture]
+public class FileNameConverterTests
+{
+    [Test]
+    [TestCase("", "")]
+    [TestCase("example.jpg", "example.jpg")]
+    [TestCase("_example.jpg", "__example.jpg")] // Always adding an extra _ because XAML remove it thinking it is an event
+    [TestCase("_toto_toto_toto_toto.jpg", "__toto_toto_toto_toto.jpg")] // Always adding an extra _ because XAML remove it thinking it is an event
+    [TestCase("_____toto_toto_toto_toto.jpg", "______toto_toto_toto_toto.jpg")] // Always adding an extra _ because XAML remove it thinking it is an event
+    public void Convert_InputStringNotNull_ReturnsFormattedFileName(string name, string expected)
+    {
+        FileNameConverter fileNameConverter = new();
+        Asset asset = new()
+        {
+            FileName = name,
+        };
+        object? parameter = null;
+
+        string result = (string)fileNameConverter.Convert(asset.FileName, typeof(Asset), parameter!, CultureInfo.InvariantCulture);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Convert_InputStringNull_ReturnsNull()
+    {
+        FileNameConverter fileNameConverter = new();
+        string? fileName = null;
+        object? parameter = null;
+
+        var result = fileNameConverter.Convert(fileName!, typeof(string), parameter!, CultureInfo.InvariantCulture);
+
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public void Convert_InputValueType_ReturnsInputValue()
+    {
+        FileNameConverter fileNameConverter = new();
+        var input = 123;
+        object? parameter = null;
+
+        var result = fileNameConverter.Convert(input, typeof(int), parameter!, CultureInfo.InvariantCulture);
+
+        Assert.AreEqual(input, result);
+    }
+
+    [Test]
+    public void ConvertBack_ThrowsNotImplementedException()
+    {
+        FileNameConverter fileNameConverter = new();
+        Type? targetType = null;
+
+        Assert.Throws<NotImplementedException>(() => fileNameConverter.ConvertBack("toto.jpg", typeof(string), targetType!, new CultureInfo("en-US")));
+    }
+}
