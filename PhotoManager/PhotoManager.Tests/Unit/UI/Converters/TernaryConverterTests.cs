471591e2bbaeae125db0cc099e606b6c0551be0a
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2023-08-12 21:53:04 +0200

    Improving TernaryConverterTests to be more accurate

---
+++ PhotoManager/PhotoManager.Tests/Unit/UI/Converters/TernaryConverterTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using PhotoManager.Domain;
+using PhotoManager.UI.Converters;
+using System.Globalization;
+
+namespace PhotoManager.Tests.Unit.UI.Converters;
+
+[TestFixture]
+public class TernaryConverterTests
+{
+    [Test]
+    [TestCase(true, "Message", "Message")]
+    [TestCase(false, null, null)]
+    public void Convert_AssetCorrupted_ReturnsValue(bool assertion, string message, string expected)
+    {
+        TernaryConverter ternaryConverter = new();
+        Asset asset = new()
+        {
+            FileName = "toto",
+            IsAssetCorrupted = assertion,
+            AssetCorruptedMessage = message,
+        };
+
+        var converterParameters = new object[] { asset.IsAssetCorrupted, asset.AssetCorruptedMessage };
+        object? parameter = null;
+
+        string? result = (string?)ternaryConverter.Convert(converterParameters, typeof(object[]), parameter!, CultureInfo.InvariantCulture);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    [TestCase(true, "Message", "Message")]
+    [TestCase(false, null, null)]
+    public void Convert_AssetRotated_ReturnsValue(bool assertion, string message, string expected)
+    {
+        TernaryConverter ternaryConverter = new();
+        Asset asset = new()
+        {
+            FileName = "toto",
+            IsAssetRotated = assertion,
+            AssetRotatedMessage = message,
+        };
+
+        var converterParameters = new object[] { asset.IsAssetRotated, asset.AssetRotatedMessage };
+        object? parameter = null;
+
+        string? result = (string?)ternaryConverter.Convert(converterParameters, typeof(object[]), parameter!, CultureInfo.InvariantCulture);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void ConvertBack_ThrowsNotImplementedException()
+    {
+        TernaryConverter ternaryConverter = new();
+        object? value = null;
+        Type[]? targetTypes = null;
+        object? parameter = null;
+
+        Assert.Throws<NotImplementedException>(() => ternaryConverter.ConvertBack(value!, targetTypes!, parameter!, CultureInfo.InvariantCulture));
+    }
+}
