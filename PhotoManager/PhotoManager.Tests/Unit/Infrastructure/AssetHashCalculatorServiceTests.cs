06476c5391ee5e0e6045b1209de4c55e45e8d3db
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2023-08-13 18:43:34 +0200

    AssetHashCalculatorServiceTests

---
+++ PhotoManager/PhotoManager.Tests/Unit/Infrastructure/AssetHashCalculatorServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using PhotoManager.Infrastructure;
+using System.IO;
+
+namespace PhotoManager.Tests.Unit.Infrastructure;
+
+[TestFixture]
+public class AssetHashCalculatorServiceTests
+{
+    private string? dataDirectory;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        var directoryName = Path.GetDirectoryName(typeof(AssetHashCalculatorServiceTests).Assembly.Location) ?? "";
+        dataDirectory = Path.Combine(directoryName, "TestFiles");
+    }
+
+    // The cases for imageBytes null, empty or invalid and filePath null or invalid are already handled in the HashingHelperTests
+    [Test]
+    [TestCase("Image 1.jpg")]
+    [TestCase("Image 1_90_deg.jpg")]
+    [TestCase("Image 1_180_deg.jpg")]
+    [TestCase("Image 1_270_deg.jpg")]
+    [TestCase("Image 8.jpeg")]
+    [TestCase("Image 10 portrait.png")]
+    [TestCase("Homer.gif")]
+    [TestCase("Image_11.heic")]
+    public void CalculateHash_ValidImageBytesAndFilePath_ReturnsHash(string fileName)
+    {
+        var filePath = Path.Combine(dataDirectory!, fileName);
+        byte[] imageBytes = File.ReadAllBytes(filePath);
+        AssetHashCalculatorService assetHashCalculatorService = new();
+
+        string hash = assetHashCalculatorService.CalculateHash(imageBytes, filePath);
+
+        Assert.IsInstanceOf<string>(hash);
+        Assert.False(string.IsNullOrWhiteSpace(hash));
+    }
+}
