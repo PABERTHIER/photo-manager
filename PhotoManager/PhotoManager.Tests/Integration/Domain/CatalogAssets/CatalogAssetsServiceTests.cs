4b617a946374825962c139b5aefb38c4c19b933f
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-09-30 22:10:20 +0200

    Adding TearDown for instance reset

--- PhotoManager/PhotoManager.Tests/Integration/Domain/CatalogAssets/CatalogAssetsServiceTests.cs
+++ PhotoManager/PhotoManager.Tests/Integration/Domain/CatalogAssets/CatalogAssetsServiceTests.cs
@@ -206,6 +206,14 @@ public class CatalogAssetsServiceTests
         };
     }
 
+    [TearDown]
+    public void Cleanup()
+    {
+        _userConfigurationService = null;
+        _testableAssetRepository = null;
+        _catalogAssetsService = null;
+    }
+
     private void ConfigureCatalogAssetService(int catalogBatchSize, string assetsDirectory, int thumbnailMaxWidth, int thumbnailMaxHeight, bool usingDHash, bool usingMD5Hash, bool usingPHash, bool analyseVideos)
     {
         Mock<IConfigurationRoot> configurationRootMock = new();
