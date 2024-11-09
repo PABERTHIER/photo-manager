edb24c89daad531b6816e62e6fd5aed14755c461
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-11-09 21:09:49 +0100

    Fix date update execution order for corruption

--- PhotoManager/PhotoManager.Tests/Integration/UI/ViewModels/ApplicationVM/ApplicationViewModelNotifyCatalogChangeTests.cs
+++ PhotoManager/PhotoManager.Tests/Integration/UI/ViewModels/ApplicationVM/ApplicationViewModelNotifyCatalogChangeTests.cs
@@ -3145,14 +3145,14 @@ public class ApplicationViewModelNotifyCatalogChangeTests
 
             // Second sync
 
-            _asset2Temp.ThumbnailCreationDateTime = DateTime.Now; // Because recreated with CreateInvalidImage()
-            File.SetLastWriteTime(imagePath1ToCopy, DateTime.Now.AddDays(10));
-
             // Corrupt image
             File.Copy(imagePath1ToCopy, imagePath1ToCopyTemp);
             ImageHelper.CreateInvalidImage(imagePath1ToCopyTemp, imagePath1ToCopy);
             File.Delete(imagePath1ToCopyTemp);
             Assert.IsTrue(File.Exists(imagePath1ToCopy));
+
+            _asset2Temp!.ThumbnailCreationDateTime = DateTime.Now.AddMinutes(-10); // Because recreated with CreateInvalidImage() + minus 10 min to simulate update
+            File.SetLastWriteTime(imagePath1ToCopy, DateTime.Now);
 
             List<string> assetPathsUpdated = [];
             assetPaths.ForEach(assetPathsUpdated.Add);
