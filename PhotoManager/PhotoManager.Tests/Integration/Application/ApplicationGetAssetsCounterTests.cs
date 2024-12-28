ff588732ec01c75a73aea30d220bb7c20e2ad22a
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-12-28 15:38:12 +0100

    ApplicationGetAssetsCounterTests

---
+++ PhotoManager/PhotoManager.Tests/Integration/Application/ApplicationGetAssetsCounterTests.cs
@@ -0,0 +1,374 @@
+﻿using Reactive = System.Reactive;
+
+namespace PhotoManager.Tests.Integration.Application;
+
+[TestFixture]
+public class ApplicationGetAssetsCounterTests
+{
+    private string? _dataDirectory;
+    private string? _databaseDirectory;
+    private string? _databasePath;
+    private readonly DateTime _expectedFileModificationDateTime = new (2024, 06, 07, 08, 54, 37);
+    private const string DATABASE_END_PATH = "v1.0";
+
+    private PhotoManager.Application.Application? _application;
+    private AssetRepository? _assetRepository;
+    private Database? _database;
+    private Mock<IStorageService>? _storageServiceMock;
+
+    private Asset? _asset1;
+    private Asset? _asset2;
+    private Asset? _asset3;
+    private Asset? _asset4;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _dataDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestFiles");
+        _databaseDirectory = Path.Combine(_dataDirectory, "DatabaseTests");
+        _databasePath = Path.Combine(_databaseDirectory, DATABASE_END_PATH);
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _asset1 = new()
+        {
+            FolderId = Guid.Empty, // Initialised later
+            Folder = new() { Id = Guid.Empty, Path = "" }, // Initialised later
+            FileName = "Image 1_duplicate.jpg",
+            Pixel = new()
+            {
+                Asset = new() { Width = 1280, Height = 720 },
+                Thumbnail = new() { Width = 200, Height = 112 }
+            },
+            FileProperties = new()
+            {
+                Size = 29857,
+                Creation = DateTime.Now,
+                Modification = _expectedFileModificationDateTime
+            },
+            ThumbnailCreationDateTime = DateTime.Now,
+            ImageRotation = Rotation.Rotate0,
+            Hash = "1fafae17c3c5c38d1205449eebdb9f5976814a5e54ec5797270c8ec467fe6d6d1190255cbaac11d9057c4b2697d90bc7116a46ed90c5ffb71e32e569c3b47fb9",
+            Metadata = new()
+            {
+                Corrupted = new() { IsTrue = false, Message = null },
+                Rotated = new() { IsTrue = false, Message = null }
+            }
+        };
+        _asset2 = new()
+        {
+            FolderId = Guid.Empty, // Initialised later
+            Folder = new() { Id = Guid.Empty, Path = "" }, // Initialised later
+            FileName = "Image 9.png",
+            Pixel = new()
+            {
+                Asset = new() { Width = 1280, Height = 720 },
+                Thumbnail = new() { Width = 200, Height = 112 }
+            },
+            FileProperties = new()
+            {
+                Size = 126277,
+                Creation = DateTime.Now,
+                Modification = _expectedFileModificationDateTime
+            },
+            ThumbnailCreationDateTime = DateTime.Now,
+            ImageRotation = Rotation.Rotate0,
+            Hash = "bcc994c14aa314dbc2dfbf48ffd34fa628dadcd86cdb8efda113b94a9035f15956cf039f5858b74cd7f404e98f7e84d9821b39aaa6cbbdc73228fa74ad2a5c20",
+            Metadata = new()
+            {
+                Corrupted = new() { IsTrue = false, Message = null },
+                Rotated = new() { IsTrue = false, Message = null }
+            }
+        };
+        _asset3 = new()
+        {
+            FolderId = Guid.Empty, // Initialised later
+            Folder = new() { Id = Guid.Empty, Path = "" }, // Initialised later
+            FileName = "Image 9_duplicate.png",
+            Pixel = new()
+            {
+                Asset = new() { Width = 1280, Height = 720 },
+                Thumbnail = new() { Width = 200, Height = 112 }
+            },
+            FileProperties = new()
+            {
+                Size = 126277,
+                Creation = DateTime.Now,
+                Modification = _expectedFileModificationDateTime
+            },
+            ThumbnailCreationDateTime = DateTime.Now,
+            ImageRotation = Rotation.Rotate0,
+            Hash = "bcc994c14aa314dbc2dfbf48ffd34fa628dadcd86cdb8efda113b94a9035f15956cf039f5858b74cd7f404e98f7e84d9821b39aaa6cbbdc73228fa74ad2a5c20",
+            Metadata = new()
+            {
+                Corrupted = new() { IsTrue = false, Message = null },
+                Rotated = new() { IsTrue = false, Message = null }
+            }
+        };
+        _asset4 = new()
+        {
+            FolderId = Guid.Empty, // Initialised later
+            Folder = new() { Id = Guid.Empty, Path = "" }, // Initialised later
+            FileName = "Image_11.heic",
+            Pixel = new()
+            {
+                Asset = new() { Width = 3024, Height = 4032 },
+                Thumbnail = new() { Width = 112, Height = 150 }
+            },
+            FileProperties = new()
+            {
+                Size = 1411940,
+                Creation = DateTime.Now,
+                Modification = _expectedFileModificationDateTime
+            },
+            ThumbnailCreationDateTime = DateTime.Now,
+            ImageRotation = Rotation.Rotate0,
+            Hash = "f52bd860f5ad7f81a92919e5fb5769d3e86778b2ade74832fbd3029435c85e59cb64b3c2ce425445a49917953e6e913c72b81e48976041a4439cb65e92baf18d",
+            Metadata = new()
+            {
+                Corrupted = new() { IsTrue = false, Message = null },
+                Rotated = new() { IsTrue = false, Message = null }
+            }
+        };
+    }
+
+    private void ConfigureApplication(int catalogBatchSize, string assetsDirectory, int thumbnailMaxWidth, int thumbnailMaxHeight, bool usingDHash, bool usingMD5Hash, bool usingPHash, bool analyseVideos)
+    {
+        Mock<IConfigurationRoot> configurationRootMock = new();
+        configurationRootMock.GetDefaultMockConfig();
+        configurationRootMock.MockGetValue(UserConfigurationKeys.CATALOG_BATCH_SIZE, catalogBatchSize.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.ASSETS_DIRECTORY, assetsDirectory);
+        configurationRootMock.MockGetValue(UserConfigurationKeys.THUMBNAIL_MAX_WIDTH, thumbnailMaxWidth.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.THUMBNAIL_MAX_HEIGHT, thumbnailMaxHeight.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.USING_DHASH, usingDHash.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.USING_MD5_HASH, usingMD5Hash.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.USING_PHASH, usingPHash.ToString());
+        configurationRootMock.MockGetValue(UserConfigurationKeys.ANALYSE_VIDEOS, analyseVideos.ToString());
+
+        UserConfigurationService userConfigurationService = new (configurationRootMock.Object);
+
+        _storageServiceMock = new Mock<IStorageService>();
+        _storageServiceMock!.Setup(x => x.ResolveDataDirectory(It.IsAny<string>())).Returns(_databasePath!);
+        _storageServiceMock!.Setup(x => x.LoadBitmapThumbnailImage(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Returns(new BitmapImage());
+
+        _database = new (new ObjectListStorage(), new BlobStorage(), new BackupStorage());
+        _assetRepository = new (_database, _storageServiceMock!.Object, userConfigurationService);
+        StorageService storageService = new (userConfigurationService);
+        AssetHashCalculatorService assetHashCalculatorService = new (userConfigurationService);
+        AssetCreationService assetCreationService = new (_assetRepository, storageService, assetHashCalculatorService, userConfigurationService);
+        AssetsComparator assetsComparator = new();
+        CatalogAssetsService catalogAssetsService = new (_assetRepository, storageService, assetCreationService, userConfigurationService, assetsComparator);
+        MoveAssetsService moveAssetsService = new (_assetRepository, storageService, assetCreationService);
+        SyncAssetsService syncAssetsService = new (_assetRepository, storageService, assetsComparator, moveAssetsService);
+        FindDuplicatedAssetsService findDuplicatedAssetsService = new (_assetRepository, storageService, userConfigurationService);
+        _application = new (_assetRepository, syncAssetsService, catalogAssetsService, moveAssetsService, findDuplicatedAssetsService, userConfigurationService, storageService);
+    }
+
+    [Test]
+    [TestCase(false)]
+    [TestCase(true)]
+    public async Task GetAssetsCounter_CataloguedAssets_ReturnsNumberOfAssets(bool analyseVideos)
+    {
+        string outputVideoFirstFrameDirectory = Path.Combine(_dataDirectory!, "OutputVideoFirstFrame");
+
+        ConfigureApplication(100, _dataDirectory!, 200, 150, false, false, false, analyseVideos);
+
+        List<Reactive.Unit> assetsUpdatedEvents = [];
+        IDisposable assetsUpdatedSubscription = _assetRepository!.AssetsUpdated.Subscribe(assetsUpdatedEvents.Add);
+
+        try
+        {
+            int assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(0));
+
+            await _application!.CatalogAssetsAsync(_ => {});
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, analyseVideos ? Is.EqualTo(52) : Is.EqualTo(51));
+
+            Assert.That(assetsUpdatedEvents, analyseVideos ? Has.Count.EqualTo(52) : Has.Count.EqualTo(51));
+        }
+        finally
+        {
+            Directory.Delete(_databaseDirectory!, true);
+
+            if (analyseVideos)
+            {
+                Directory.Delete(outputVideoFirstFrameDirectory, true);
+            }
+
+            assetsUpdatedSubscription.Dispose();
+        }
+    }
+
+    [Test]
+    public void GetAssetsCounter_AssetsExist_ReturnsNumberOfAssets()
+    {
+        string assetsDirectory = Path.Combine(_dataDirectory!, "Duplicates\\NewFolder2");
+
+        ConfigureApplication(100, assetsDirectory, 200, 150, false, false, false, false);
+
+        List<Reactive.Unit> assetsUpdatedEvents = [];
+        IDisposable assetsUpdatedSubscription = _assetRepository!.AssetsUpdated.Subscribe(assetsUpdatedEvents.Add);
+
+        try
+        {
+            Folder folder = _assetRepository!.AddFolder(assetsDirectory);
+
+            _asset1 = _asset1!.WithFolder(folder);
+            _asset2 = _asset2!.WithFolder(folder);
+            _asset3 = _asset3!.WithFolder(folder);
+            _asset4 = _asset4!.WithFolder(folder);
+
+            Assert.That(assetsUpdatedEvents, Is.Empty);
+
+            int assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(0));
+
+            Assert.That(assetsUpdatedEvents, Is.Empty);
+
+            _assetRepository.AddAsset(_asset1!, []);
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(1));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(1));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+
+            _assetRepository.AddAsset(_asset2!, []);
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(2));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(2));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+
+            _assetRepository.AddAsset(_asset3!, []);
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(3));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(3));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[2], Is.EqualTo(Reactive.Unit.Default));
+
+            _assetRepository.DeleteAsset(assetsDirectory, _asset3.FileName);
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(2));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(4));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[2], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[3], Is.EqualTo(Reactive.Unit.Default));
+
+            _assetRepository.AddAsset(_asset4!, []);
+
+            assetsCounter = _application!.GetAssetsCounter();
+            Assert.That(assetsCounter, Is.EqualTo(3));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(5));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[2], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[3], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[4], Is.EqualTo(Reactive.Unit.Default));
+        }
+        finally
+        {
+            Directory.Delete(_databaseDirectory!, true);
+            assetsUpdatedSubscription.Dispose();
+        }
+    }
+
+    [Test]
+    public void GetAssetsCounter_NoAsset_Returns0()
+    {
+        string assetsDirectory = Path.Combine(_dataDirectory!, "TempEmptyFolder");
+
+        ConfigureApplication(100, assetsDirectory, 200, 150, false, false, false, false);
+
+        List<Reactive.Unit> assetsUpdatedEvents = [];
+        IDisposable assetsUpdatedSubscription = _assetRepository!.AssetsUpdated.Subscribe(assetsUpdatedEvents.Add);
+
+        try
+        {
+            Directory.CreateDirectory(assetsDirectory);
+
+            int assetsCounter = _application!.GetAssetsCounter();
+
+            Assert.That(assetsCounter, Is.EqualTo(0));
+
+            Assert.That(assetsUpdatedEvents, Is.Empty);
+        }
+        finally
+        {
+            Directory.Delete(_databaseDirectory!, true);
+            Directory.Delete(assetsDirectory, true);
+            assetsUpdatedSubscription.Dispose();
+        }
+    }
+
+    [Test]
+    public void GetAssetsCounter_ConcurrentAccess_AssetsAreHandledSafely()
+    {
+        string assetsDirectory = Path.Combine(_dataDirectory!, "Duplicates\\NewFolder2");
+
+        ConfigureApplication(100, assetsDirectory, 200, 150, false, false, false, false);
+
+        List<Reactive.Unit> assetsUpdatedEvents = [];
+        IDisposable assetsUpdatedSubscription = _assetRepository!.AssetsUpdated.Subscribe(assetsUpdatedEvents.Add);
+
+        try
+        {
+            Folder folder = _assetRepository!.AddFolder(assetsDirectory);
+
+            _asset1 = _asset1!.WithFolder(folder);
+            _asset2 = _asset2!.WithFolder(folder);
+            _asset3 = _asset3!.WithFolder(folder);
+            _asset4 = _asset4!.WithFolder(folder);
+
+            _assetRepository.AddAsset(_asset1!, []);
+            _assetRepository.AddAsset(_asset2!, []);
+            _assetRepository.AddAsset(_asset3!, []);
+            _assetRepository.AddAsset(_asset4!, []);
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(4));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[2], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[3], Is.EqualTo(Reactive.Unit.Default));
+
+            int assetsCounter1 = 0;
+            int assetsCounter2 = 0;
+            int assetsCounter3 = 0;
+
+            // Simulate concurrent access
+            Parallel.Invoke(
+                () => assetsCounter1 = _application!.GetAssetsCounter(),
+                () => assetsCounter2 = _application!.GetAssetsCounter(),
+                () => assetsCounter3 = _application!.GetAssetsCounter()
+            );
+
+            Assert.That(assetsCounter1, Is.EqualTo(4));
+            Assert.That(assetsCounter2, Is.EqualTo(4));
+            Assert.That(assetsCounter3, Is.EqualTo(4));
+
+            Assert.That(assetsUpdatedEvents, Has.Count.EqualTo(4));
+            Assert.That(assetsUpdatedEvents[0], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[1], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[2], Is.EqualTo(Reactive.Unit.Default));
+            Assert.That(assetsUpdatedEvents[3], Is.EqualTo(Reactive.Unit.Default));
+        }
+        finally
+        {
+            Directory.Delete(_databaseDirectory!, true);
+            assetsUpdatedSubscription.Dispose();
+        }
+    }
+}
