aaead3d84f39c20b4dad50ecd987655032db5513
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2025-01-04 23:48:20 +0100

    ApplicationGetSyncAssetsEveryXMinutesTests

---
+++ PhotoManager/PhotoManager.Tests/Integration/Application/ApplicationGetSyncAssetsEveryXMinutesTests.cs
@@ -0,0 +1,65 @@
+﻿namespace PhotoManager.Tests.Integration.Application;
+
+[TestFixture]
+public class ApplicationGetSyncAssetsEveryXMinutesTests
+{
+    private string? _dataDirectory;
+    private string? _databaseDirectory;
+    private string? _databasePath;
+    private const string DATABASE_END_PATH = "v1.0";
+
+    private PhotoManager.Application.Application? _application;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _dataDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestFiles");
+        _databaseDirectory = Path.Combine(_dataDirectory, "DatabaseTests");
+        _databasePath = Path.Combine(_databaseDirectory, DATABASE_END_PATH);
+    }
+
+    private void ConfigureApplication(string assetsDirectory, bool syncAssetsEveryXMinutes)
+    {
+        Mock<IConfigurationRoot> configurationRootMock = new();
+        configurationRootMock.GetDefaultMockConfig();
+        configurationRootMock.MockGetValue(UserConfigurationKeys.ASSETS_DIRECTORY, assetsDirectory);
+        configurationRootMock.MockGetValue(UserConfigurationKeys.SYNC_ASSETS_EVERY_X_MINUTES, syncAssetsEveryXMinutes.ToString());
+
+        UserConfigurationService userConfigurationService = new (configurationRootMock.Object);
+
+        Mock<IStorageService> storageServiceMock = new();
+        storageServiceMock.Setup(x => x.ResolveDataDirectory(It.IsAny<string>())).Returns(_databasePath!);
+        storageServiceMock.Setup(x => x.LoadBitmapThumbnailImage(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Returns(new BitmapImage());
+
+        Database database = new (new ObjectListStorage(), new BlobStorage(), new BackupStorage());
+        AssetRepository assetRepository = new (database, storageServiceMock.Object, userConfigurationService);
+        StorageService storageService = new (userConfigurationService);
+        AssetHashCalculatorService assetHashCalculatorService = new (userConfigurationService);
+        AssetCreationService assetCreationService = new (assetRepository, storageService, assetHashCalculatorService, userConfigurationService);
+        AssetsComparator assetsComparator = new();
+        CatalogAssetsService catalogAssetsService = new (assetRepository, storageService, assetCreationService, userConfigurationService, assetsComparator);
+        MoveAssetsService moveAssetsService = new (assetRepository, storageService, assetCreationService);
+        SyncAssetsService syncAssetsService = new (assetRepository, storageService, assetsComparator, moveAssetsService);
+        FindDuplicatedAssetsService findDuplicatedAssetsService = new (assetRepository, storageService, userConfigurationService);
+        _application = new (assetRepository, syncAssetsService, catalogAssetsService, moveAssetsService, findDuplicatedAssetsService, userConfigurationService, storageService);
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void GetSyncAssetsEveryXMinutes_CorrectValue_ReturnsSyncAssetsEveryXMinutesValue(bool expectedSyncAssetsEveryXMinutes)
+    {
+        ConfigureApplication(_dataDirectory!, expectedSyncAssetsEveryXMinutes);
+
+        try
+        {
+            bool syncAssetsEveryXMinutes = _application!.GetSyncAssetsEveryXMinutes();
+
+            Assert.That(syncAssetsEveryXMinutes, Is.EqualTo(expectedSyncAssetsEveryXMinutes));
+        }
+        finally
+        {
+            Directory.Delete(_databaseDirectory!, true);
+        }
+    }
+}
