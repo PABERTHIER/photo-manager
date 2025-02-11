dd598d82d0cf82a4a5efac77ec9fc94e2720a64e
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2025-02-11 23:52:36 +0100

    DateTimeAssetComparer + LongAssetComparer + StringAssetComparer

---
+++ PhotoManager/PhotoManager.Domain/Comparers/StringAssetComparer.cs
@@ -0,0 +1,16 @@
+﻿namespace PhotoManager.Domain.Comparers;
+
+public class StringAssetComparer(bool ascending, Func<Asset, string> stringSelector) : IComparer<Asset>
+{
+    public int Compare(Asset? asset1, Asset? asset2)
+    {
+        if (asset1 == null || asset2 == null)
+        {
+            throw new ArgumentNullException(asset1 == null ? nameof(asset1) : nameof(asset2));
+        }
+        
+        int result = string.Compare(stringSelector(asset1), stringSelector(asset2), StringComparison.Ordinal);
+
+        return ascending ? result : -result;
+    }
+}
