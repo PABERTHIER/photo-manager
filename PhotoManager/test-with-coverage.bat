30a5f9d993055a9d24830f9887a1ccd91dd752f1
Pierre-Antoine Berthier <39381248+PABERTHIER@users.noreply.github.com>
2025-12-27 21:31:07 +0100

    🏗️ Migrate SLN to SLNX (#52)

--- PhotoManager/test-with-coverage.bat
+++ PhotoManager/test-with-coverage.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q .\TestResults
 rmdir /S /Q .\PhotoManager.Tests\TestResults
 
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromCodeCoverageAttribute" PhotoManager.sln --collect:"XPlat Code Coverage"
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromCodeCoverageAttribute" PhotoManager.slnx --collect:"XPlat Code Coverage"
 
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path Tools
 .\Tools\reportgenerator.exe -reports:PhotoManager.Tests\TestResults\*\coverage.cobertura.xml -targetdir:.\TestResults\
